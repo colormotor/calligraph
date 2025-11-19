649813966a4b434766101a9dc54432535c50e2f4
colormotor <drand48@gmail.com>
2025-11-19 17:17:26 +0000

    ...

--- examples/demo_slm_gml_01.py
+++ examples/demo_slm_gml_01.py
@@ -23,7 +23,8 @@ from calligraph import (plut,
                         segmentation,
                         spline_losses,
                         image_losses,
-                        contour_histogram)
+                        
+                        )
 
 import pdb
 
@@ -41,6 +42,8 @@ def params():
 
     output_path = './generated' #os.path.expanduser('~/Dropbox/transfer_box/data/calligraph/outputs/')
 
+    filename = './data/gml/49867.json'
+    
     lr_shape = 2.0
     lr_width = 0.3
     lr_delta_t = 0.05
@@ -67,28 +70,14 @@ saver = util.SaveHelper(__file__, output_path, use_wandb=cfg.headless,
                         dropbox_folder=cfg.dropbox_folder,
                         cfg=cfg)
 
-from py5canvas import Canvas
-
-#S = files.load_gml('./data/janke_gml_1.json') #
-S = fs.load_gml('./data/gml/49867.json') #janke_gml_1.json') #[1:2]
-#S = files.load_json(os.path.expanduser('~/Dropbox/develop_box/emot_drawing/caz.mp4.json'))
+S = fs.load_gml(cfg.filename)
 S = geom.transform_to_rect(S, geom.make_rect(0, 0, cfg.size, cfg.size), padding=20)
 
 fig = plut.figure_pixels(cfg.size, cfg.size)
 plut.stroke(S, 'k', lw=cfg.stroke_w*2)
-input_img = plut.figure_image(close=True)
-
-# c = Canvas(cfg.size, cfg.size)
-# c.background(255)
-# c.stroke(0)
-# c.no_fill()
-# c.stroke_weight(cfg.stroke_w*2)
-# c.shape(S)
-# input_img = c.Image().convert('L')
-#P = geom.shapes.star(cfg.size/2.2, center=[cfg.size/2, cfg.size/2])
-#P = geom.close(P)
-#pad = 50
-#P = np.random.uniform(pad, cfg.size-pad, (15, 2))
+plut.setup(box=geom.rect(0, 0, cfg.size, cfg.size))
+input_img = plut.figure_image(close=True).convert('L')
+# Simplify initial paths to retrieve action plan keypoints
 startup_paths = [dce.dce(P, 2.5, perimeter=cfg.size) for P in S]
 
 img = np.array(input_img)/255
@@ -98,9 +87,6 @@ h, w = img.shape
 ##############################################
 # Settings
 verbose = False
-ref_size = w*cfg.ref_size_factor
-offset_variance = [ref_size, ref_size]
-
 np.random.seed(133)
 
 ##############################################
@@ -130,47 +116,46 @@ for P in startup_paths:
     paths.append(path)
 
 
-Opt = torch.optim.Adam #Adam
-Opt = lambda params, lr: torch.optim.Adam(params, lr, betas=(0.9, 0.9), eps=1e-6)
+# Opt = torch.optim.Adam #Adam
+# Opt = lambda params, lr: torch.optim.Adam(params, lr, betas=(0.9, 0.9), eps=1e-6)
 
+##############################################
+# Optimization
 
-optimizers = [Opt(scene.get_points(), lr=cfg.lr_shape),
-              Opt(scene.get_params('Delta_t'), lr=cfg.lr_delta_t)]
+params = [(scene.get_points(), cfg.lr_shape),
+          (scene.get_params('Delta_t'), cfg.lr_delta_t)]
 if cfg.lr_Ac > 0:
-    optimizers += [
-              Opt(scene.get_params('Ac'), lr=cfg.lr_Ac),
+    params += [
+              (scene.get_params('Ac'), cfg.lr_Ac),
               ]
 
 if cfg.lr_delta > 0:
-    optimizers += [
-              Opt(scene.get_params('delta'), lr=cfg.lr_delta),
+    params += [
+              (scene.get_params('delta'), cfg.lr_delta),
               ]
 
 
-warmup = 5
-#schedulers = []
-schedulers = [get_cosine_schedule_with_warmup(opt, warmup, int(cfg.num_opt_steps*cfg.schedule_decay_factor)) for opt in optimizers]
+opt = diffvg_utils.SceneOptimizer(scene,
+                                  params=params,
+                                  num_steps=cfg.num_opt_steps,
+                                  lr_min_scale=0.1)
+
+
 ##############################################
 # Losses
 
-
-losses = util.MultiLoss(verbose=verbose)
-#losses.add('mse',
-#           image_losses.MultiscaleMSELoss(rgb=False), cfg.mse_w)
-mse = image_losses.MultiscaleMSELoss(rgb=False, debug=True)
-losses.add('mse',
-           mse, cfg.mse_w)
+opt.add_loss('mse', image_losses.MultiscaleMSELoss(rgb=False), cfg.mse_w,
+            inputs=('im', 'input_img', 'mse_mul'))
 
 def smooth_loss(paths):
-    #return sum([path.endt_norm*100 for path in paths])/len(paths)
     return sum([path.time_cost(50)*100 for path in paths])/len(paths)
-    #return sum([path.jerk(10, ref_size=w) for path in paths])/len(paths)
 
-losses.add('smooth',
-               smooth_loss, cfg.smoothing_w)
+opt.add_loss('smooth',
+               smooth_loss, cfg.smoothing_w,
+             ('shapes',))
 
-losses.add('bbox',
-           spline_losses.make_bbox_loss(geom.make_rect(0, 0, w, h)), 1.0)
+opt.add_loss('bbox', spline_losses.make_bbox_loss(geom.rect(0, 0, w, h)), 1.0,
+            inputs=('points',))
 
 ##############################################
 # Begin visualization and optimize
@@ -187,34 +172,21 @@ render_shapes = []
 def frame(step):
     global first_frame, render_shapes, start_time
 
-    for opt in optimizers:
-       opt.zero_grad()
+    opt.zero_grad()
 
     # Rasterize
     with util.perf_timer('render', verbose=verbose):
-        im = scene.render(background_image, num_samples=2)[:,:,0].to(device)
-
-    im_no_gamma = im
-    if cfg.alpha < 1:
-        im = im**300 # # ** gamma
+        im = opt.render(background_image)[:,:,0]
 
     if cfg.smoothing_warmup > 0:
         smooth_w = (min(step, cfg.smoothing_warmup)/cfg.smoothing_warmup)*cfg.smoothing_w
-        losses.replace_weights(smooth=smooth_w)
-    # Losses (see weights above)
-    loss = losses(
-        mse=(im_no_gamma, target_tensor, cfg.mse_mul),
-        smooth=(scene.shapes,),
-        bbox=(scene.shapes,),
-    )
+        opt.losses.replace_weights(smooth=smooth_w)
 
-    if cfg.num_opt_steps > 1:
-        with util.perf_timer('Opt step', verbose=verbose):
-            loss.backward()
-            for opt in optimizers:
-                opt.step()
-            for sched in schedulers:
-                sched.step()
+    opt.step(im=im,
+             points=scene.get_points(),
+             input_img=target_img,
+             shapes=scene.shapes,
+             mse_mul=cfg.mse_mul)
 
     # Constrain
     with torch.no_grad():
@@ -234,10 +206,7 @@ def frame(step):
     plt.subplot(gs[0,0])
     plt.title('Startup')
     amt = 0.7
-    #mseim =(mse.target.detach().cpu().numpy() + mse.im.detach().cpu().numpy())/2
     plt.imshow(first_frame*amt + target_img*(1-amt), cmap='gray', vmin=0, vmax=1)
-    #plut.stroke(startup_paths, 'r', lw=0.5)
-    #plt.imshow(mseim, cmap='gray')
 
     # for i, path in enumerate(paths):
     #     Q = path.param('points').detach().cpu().numpy()
@@ -257,46 +226,11 @@ def frame(step):
         dslm.plot_action_plan(P, delta)
     plut.setup(box=geom.make_rect(0, 0, w, h), axis=True)
 
-    # plt.imshow(background_image, cmap='gray', vmin=0, vmax=1)
-    # blur_target = losses.items['mse'][0].blur_target.detach().cpu().numpy()
-    # #plt.imshow(np.array(img)/255, cmap='gray', alpha=1)
-    # plt.imshow(blur_target, cmap='gray', vmin=0, vmax=1, alpha=0.7)
 
-    # #plut.stroke(startup_path, 'r', lw=1)
-    # step_skip = 1
-
-    # if step % step_skip == 0: #with util.perf_timer('Thick curves', verbose=verbose) :
-    #     render_shapes = []
-    #     for i, path in enumerate(paths):
-    #         Q = path.param('points').detach().cpu().numpy()
-    #         X = path.samples(len(Q)*10).detach().cpu().numpy()
-    #         #X[:,2] *= 0.75
-    #         if cfg.fill: # and not overlap:
-    #             plut.fill(X[:,:2], 'k', alpha=cfg.alpha*cfg.image_alpha)
-    #             plut.stroke(X[:,:2], 'k')
-    #         else:
-    #             # if not cfg.vary_width:
-    #             #     X[:,2] *= 0.5
-    #             # S = geom.thick_curve(X, add_cap=False)
-    #             # render_shapes.append(S)
-    #             plut.stroke(X[:,:2], 'k', lw=cfg.stroke_w)
-    #         #plut.fill(S, 'k')
-
-    # if render_shapes:
-    #     for S in render_shapes:
-    #         plut.fill(S, 'k')
-
-    #plt.legend()
-    #plut.setup(box=geom.make_rect(0, 0, w, h))
     plt.subplot(gs[1,:])
     plt.title('Loss')
-    for i, (key, kloss) in enumerate(losses.losses.items()):
-        if key=='total' or not losses.has_loss(key):
-            # There is a bug in 'total'
-            continue
-        plt.plot(kloss, label='%s:%.4f'%(key, kloss[-1]))
-    plt.legend()
-
+    opt.plot(50)
+    
     ax = plt.subplot(gs[2,:])
     plt.title('Strokes')
     lens = [path.num_points() for path in scene.shapes]
